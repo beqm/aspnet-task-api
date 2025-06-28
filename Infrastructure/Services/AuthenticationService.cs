196f2307a9c66dbb4f10d3519169042f9638fcc6
beqm <brenomartinsmb@gmail.com>
2025-06-28 16:52:13 -0300

    feat: service for jwt

---
+++ Infrastructure/Services/AuthenticationService.cs
@@ -0,0 +1,53 @@
+using System.Text;
+using Domain.Models;
+using Domain.Interfaces;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string hash, string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+
+    public string GenerateJwtToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Role, user.Role),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? string.Empty));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
